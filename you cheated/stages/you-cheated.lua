f042a3f40904b6df081b4353ae9901fa60cd25ec
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-08-21 03:23:02 -0400

    Add files via upload

---
+++ you cheated/stages/you-cheated.lua
@@ -0,0 +1,10 @@
+--created with Super_Hugo's Stage Editor v1.6.3
+
+function onCreate()
+
+	makeLuaSprite('obj1', 'you_cheated', -541, -309)
+	setObjectOrder('obj1', 0)
+	scaleObject('obj1', 4.8, 4.8)
+	addLuaSprite('obj1', true)
+	
+end
