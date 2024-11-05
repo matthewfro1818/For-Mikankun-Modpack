756e41fc31750bc6d3cb45806a79ff879c4d4639
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-11-05 18:59:01 -0500

    Create whitebg.lua

---
+++ FalseParadiseMod/stages/whitebg.lua
@@ -0,0 +1,10 @@
+function onCreate()
+	-- background shit
+	
+	makeLuaSprite('white', 'white', -600, -300);
+	setScrollFactor('white', 0.9, 0.9);
+
+	addLuaSprite('white', false);
+	
+	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
+end
